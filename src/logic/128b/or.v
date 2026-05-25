a08268e55e97b3c36f52a26aa1a8142f919468fd
sparkles-laurel <sparkles@getconfigured.org>
2026-05-25 19:04:33 +0300

    add or and xor instructions

---
+++ src/logic/128b/or.v
@@ -0,0 +1,31 @@
+// This source describes Open Hardware and is licensed under the CERN-OHL-S v2.0
+// SPDX-License-Identifier: CERN-OHL-S-2.0
+//
+// You may redistribute and modify this source and make products using it under
+// the terms of the CERN-OHL-S v2.0 (https://ohwr.org/cern_ohl_s_v2.txt)
+//
+// Unless required by applicable law or agreed to in writing, this source
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+//
+// See the license for the specific language governing permissions and limitations.
+
+module or_128 (
+        input wire [127:0] a,
+        input wire [127:0] b,
+        output wire [127:0] out
+    );
+
+    genvar i;
+
+    generate
+        for (i = 0; i < 127; i = i + 1) begin
+
+            or_gate u_or_gate(
+                         .a   	(a[i]    ),
+                         .b   	(b[i]    ),
+                         .out 	(out[i]  )
+                     );
+        end
+    endgenerate
+
+endmodule; //and
---
+++ src/logic/128b/xor.v
@@ -0,0 +1,31 @@
+// This source describes Open Hardware xor is licensed under the CERN-OHL-S v2.0
+// SPDX-License-Identifier: CERN-OHL-S-2.0
+//
+// You may redistribute xor modify this source and make products using it under
+// the terms of the CERN-OHL-S v2.0 (https://ohwr.org/cern_ohl_s_v2.txt)
+//
+// Unless required by applicable law or agreed to in writing, this source
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+//
+// See the license for the specific language governing permissions xor limitations.
+
+module xor_128 (
+        input wire [127:0] a,
+        input wire [127:0] b,
+        output wire [127:0] out
+    );
+
+    genvar i;
+
+    generate
+        for (i = 0; i < 127; i = i + 1) begin
+
+            xor_gate u_and_gate(
+                         .a   	(a[i]    ),
+                         .b   	(b[i]    ),
+                         .out 	(out[i]  )
+                     );
+        end
+    endgenerate
+
+endmodule; //xor
